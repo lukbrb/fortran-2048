1438fba38ead4c3e008d9fbf91af6165f939e354
lukbrb <lucasbarbr@outlook.com>
2024-01-09 18:44:23 +0100

    début logique pour bouger les nombres

--- src/game.f90
+++ src/game.f90
@@ -1,9 +1,26 @@
 module game
+    use raylib
     implicit none
     
     private
 
     public :: board_size_cl
     integer, parameter :: board_size_cl = 4 ! On prend un carré 4x4 pour la grille
+contains
 
+    subroutine move_numbers(board, keypressed)
+        integer, intent(inout) :: board(board_size_cl, board_size_cl)
+        integer, intent(in) :: keypressed
+
+        select case (keypressed)
+        case (KEY_RIGHT)
+            call update_board(board, 1, 0)
+        case (KEY_LEFT)
+            call update_board(board, -1, 0)
+        case (KEY_DOWN)
+            call update_board(board, 0, -1)
+        case (KEY_UP)
+            call update_board(board, 0, 1)
+        end select
+    end subroutine move_numbers
 end module game
