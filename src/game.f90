7677f46f4a9b0e3ed7809ef712fc43dcb626fd84
lukbrb <lucasbarbr@outlook.com>
2024-01-09 18:49:16 +0100

    Update game.f90

--- src/game.f90
+++ src/game.f90
@@ -23,4 +23,9 @@ contains
             call update_board(board, 0, 1)
         end select
     end subroutine move_numbers
+
+    subroutine update_board(board, x_shift, y_shift)
+        integer, intent(inout) :: board(board_size_cl, board_size_cl)
+        integer, intent(in) :: x_shift, y_shift
+    end subroutine update_board
 end module game
