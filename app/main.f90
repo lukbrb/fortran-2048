93da2488df54f8c9cf6dcc7d0be53dad9fc96bbd
lukbrb <lucasbarbr@outlook.com>
2024-03-01 19:49:32 +0100

    correction bogue sur ecriture score
    Si la partie était rejouée, le score n'était pas écrit

--- app/main.f90
+++ app/main.f90
@@ -9,7 +9,7 @@ program main
 
   integer(kind=c_int) :: width, height, fps
   real(c_float) :: dt
-  integer :: board(4, 4)
+  integer :: board(board_size_cl, board_size_cl)
   integer(c_int) :: keypressed
   real    :: board_x_px, board_y_px, board_boundary_width, board_boundary_height, board_size_px, cell_size_px
   logical :: can_board_move = .true.
@@ -28,7 +28,6 @@ program main
   call init_window(width, height, "FORTRAN 2048"// c_null_char)
   call set_target_fps(fps)
 
-  ! game_font = load_font_ex("fonts/Alegreya-Regular.ttf"//C_NULL_CHAR, font_size, C_NULL_PTR, 0)
   do while(.not. window_should_close())
     call begin_drawing()
       call begin_screen_fitting()
@@ -55,12 +54,11 @@ program main
 
       cell_size_px = board_size_px/board_size_cl
       score_actuel = get_score(board)
+      score_record = get_record(score_actuel, score_record)
       call render_board(board_x_px, board_y_px, board_size_px, board, restart_the_game)
-      call display_score_boxes(score_actuel, get_record(score_actuel, score_record), board_x_px, board_y_px, &
+      call display_score_boxes(score_actuel, score_record, board_x_px, board_y_px, &
                                board_size_px)
-      ! if (restart_button_clicked()) then
-      !   board = init_board()
-      ! end if
+
       if (keypressed /= 0) then
         can_board_move = board_moved(board, keypressed)
         if (can_board_move) then
@@ -80,6 +78,7 @@ program main
       if (restart_the_game) then
         board = init_board()
         restart_the_game = .false.
+        call write_file(get_record(score_record, get_score(board)), path) ! Le score doit être écrit aussi si la partie est recommencée
       end if
       call end_screen_fitting()
     call end_drawing()
